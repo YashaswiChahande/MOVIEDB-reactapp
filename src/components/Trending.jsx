a232b93bd1a8f162c64c3f0856cb8966128a28c4
Yashaswi Chahande <chahandeyashaswi@gmail.com>
2025-02-25 17:04:50 +0530

    MovieDB TrendInfScrollfinp1

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -4,34 +4,47 @@ import Topnav from './partials/Topnav';
 import Dropdown from './partials/Dropdown';
 import axios from '../utils/axios';
 import Cards from './partials/Cards';
+import Loading from './Loading';
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 const Trending = () => {
+
+    
     const navigate = useNavigate();
-    const [category, setcategory] = useState("all")
-    const [duration, setduration] = useState("day")
-    const [trending, settrending] = useState([])
+    const [category, setcategory] = useState("all");
+    const [duration, setduration] = useState("day");
+    const [trending, settrending] = useState([]);
+    const [page, setpage] = useState(1);
 
     const GetTrending = async () => {
         try {
-          const { data } = await axios.get(`/trending/${category}/${duration}`);
-            settrending(data.results);
-        } catch (error) {
-            console.log("Error: ", error);
-        }
+          const { data } = await axios.get(
+            `/trending/${category}/${duration}`
+
+          );
+            // settrending(data.results);
+            settrending((prevstate) => [...prevstate, ...data.results])
+
+            setpage(page + 1)
+            console.log(data)
+
+            } catch (error) {
+                console.log("Error: ", error);
+            }
     };
 
     useEffect(() => {
        GetTrending();
     }, [category, duration]);
 
-  return (
-    <div className='px-[3%] w-screen h-screen overflow-hidden overflow-y-auto'>
-        <div className=' w-full flex items-center justify-between '>
+  return trending.length > 0 ? (
+    <div className=' w-screen h-screen '>
+        <div className='px-[5%] w-full flex items-center justify-between '>
             <h1 className=' text-2xl font-semibold text-zinc-400'>
             <i
                         onClick={() => navigate(-1)}
                         className="hover:text-[#6556CD] ri-arrow-left-line" >
-            </i>
+            </i>{" "}
                 Trending
             </h1>
             <div className='flex items-center w-[80%]'>
@@ -48,14 +61,21 @@ const Trending = () => {
                     func={(e)=> setduration(e.target.value)}>
                 </Dropdown>
             </div>
-
-            
-
         </div>
 
-        <Cards data={trending} title={category}/>
+        <InfiniteScroll
+            dataLength={trending.length}
+            next={GetTrending}
+            hasMore={true}
+            loader={<h1>Loading....</h1>}
+        >
+            <Cards data={trending} title={category}/>
+        </InfiniteScroll>
+       
     </div>
-  )
+  ) : (
+    <Loading/>
+    )
 }
 
 export default Trending
--- src/components/partials/Cards.jsx
+++ src/components/partials/Cards.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Cards = ({ data, title }) => {
   return (
-    <div className='flex flex-wrap w-full h'>
+    <div className='flex flex-wrap w-full h-full p-[5%] bg-[#1f1e24] '>
         {data.map((c, i)=> (
             
             <Link className="w-[25vh] mr-[5%] mb-[5%]" key={i} >
